eac2e4bf5d0f1c032a56753e937791cc0257c5c1
John Wanjema <jonwanjema@gmail.com>
2024-02-15 22:14:49 -0800

    implement login function

--- pages/index.vue
+++ pages/index.vue
@@ -1,86 +1,94 @@
 <template>
-  <div id="page-container" class="main-content-boxed">
-      <main id="main-container">
-          <div class="bg-gd-dusk">
-              <div class="hero-static content content-full bg-body-extra-light">
-                  <div class="py-4 px-1 text-center mb-4">
-                      <a class="link-fx fw-bold" href="index.html">
-                          <i class="fa fa-fire"></i>
-                          <span class="fs-4 text-body-color">code</span><span class="fs-4">base</span>
-                      </a>
-                      <h1 class="h3 fw-bold mt-5 mb-2">Welcome to Your Dashboard</h1>
-                      <h2 class="h5 fw-medium text-muted mb-0">Please sign in</h2>
-                  </div>
-                  <div class="row justify-content-center px-1">
-                      <div class="col-sm-8 col-md-6 col-xl-4">
-                          <form class="js-validation-signin" action="be_pages_auth_all.html" method="POST">
-                              <div class="form-floating mb-4">
-                                  <input type="text" class="form-control" id="login-username" name="login-username" placeholder="Enter your username" />
-                                  <label class="form-label" for="login-username">Username</label>
-                              </div>
-                              <div class="form-floating mb-4">
-                                  <input type="password" class="form-control" id="login-password" name="login-password" placeholder="Enter your password" />
-                                  <label class="form-label" for="login-password">Password</label>
-                              </div>
-                              <div class="row g-sm mb-4">
-                                  <div class="col-12 mb-2">
-                                      <button type="submit" class="btn btn-lg btn-alt-primary w-100 py-3 fw-semibold">
-                                          Sign In
-                                      </button>
-                                  </div>
-                                  <div class="col-sm-6 mb-1">
-                                      <a class="btn btn-alt-secondary w-100" href="op_auth_signup.html">
-                                          New Account
-                                      </a>
-                                  </div>
-                                  <div class="col-sm-6 mb-1">
-                                      <a class="btn btn-alt-secondary w-100" href="op_auth_reminder.html">
-                                          Forgot password
-                                      </a>
-                                  </div>
-                              </div>
-                          </form>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </main>
-  </div>
+    <div id="page-container" class="main-content-boxed side-trans-enabled">
+        <!-- Main Container -->
+        <main id="main-container" style="min-height: 182px;">
+            <!-- Page Content -->
+            <div class="bg-gd-dusk">
+                <div class="hero-static content content-full bg-white js-appear-enabled animated fadeIn" data-toggle="appear">
+                    <!-- Header -->
+                    <div class="py-30 px-5 text-center">
+                        <a class="link-effect font-w700" href="index.html">
+                            <i class="si si-fire"></i>
+                            <span class="font-size-xl text-primary-dark">code</span><span class="font-size-xl">base</span>
+                        </a>
+                        <h1 class="h2 font-w700 mt-50 mb-10">Welcome to Your Dashboard</h1>
+                        <h2 class="h4 font-w400 text-muted mb-0">Please sign in</h2>
+                    </div>
+                    <!-- END Header -->
+
+                    <!-- Sign In Form -->
+                    <div class="row justify-content-center px-5">
+                        <div class="col-sm-8 col-md-6 col-xl-4">
+                            <form @submit.prevent="login">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <div class="form-material floating">
+                                            <input v-model="username" required type="text" class="form-control" id="login-username" name="login-username" />
+                                            <label for="login-username">Username</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <div class="form-material floating">
+                                            <input v-model="password" required type="password" class="form-control" id="login-password" name="login-password" />
+                                            <label for="login-password">Password</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row gutters-tiny">
+                                    <div class="col-12 mb-10">
+                                        <button type="submit" class="btn btn-block btn-hero btn-noborder btn-rounded btn-alt-primary"><i class="si si-login mr-10"></i> Sign In</button>
+                                    </div>
+                                    <div class="col-sm-6 mb-5">
+                                        <a class="btn btn-block btn-noborder btn-rounded btn-alt-secondary" href="op_auth_signup.html"> <i class="fa fa-plus text-muted mr-5"></i> New Account </a>
+                                    </div>
+                                    <div class="col-sm-6 mb-5">
+                                        <a class="btn btn-block btn-noborder btn-rounded btn-alt-secondary" href="op_auth_reminder.html"> <i class="fa fa-warning text-muted mr-5"></i> Forgot password </a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- END Sign In Form -->
+                </div>
+            </div>
+            <!-- END Page Content -->
+        </main>
+        <!-- END Main Container -->
+    </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            // form: new Form({
-            //     username:'',
-            //     password:'',
-            // }),
-        }
-    },
-    // watch: {
-    //     currentPage: {
-    //         handler: function(value) {
-    //             this.getAFHSMessages(value)
-    //         }
-    //     }
-    // },
-    methods: {
-        login() {
-            axios.get('/api/afhs').then(({ data }) => {
-                this.afhs = data.data;
-                if (data.data.length > 0) { 
-                    this.form.afh_id = data.data[0].id;
-                    this.getAFHStaff();
-                    this.getAFHSMessages();
-                }
-            }).catch((error) => {
-                console.log(error);
-            });
+    export default {
+        data() {
+            return {
+                username: "",
+                password: "",
+                ip: "",
+            };
         },
-    },
-    created() {
-      console.log('users')
-    }
-}
+        // watch: {
+        //     currentPage: {
+        //         handler: function(value) {
+        //             this.getAFHSMessages(value)
+        //         }
+        //     }
+        // },÷
+        methods: {
+            async login() {
+                await this.$http
+                    .$post("https://apptest.dekioz.co.ke/api/Auth/login", {
+                        user: this.username,
+                        password: this.password,
+                    })
+                    .then((response) => {
+                        console.log(response.accessToken);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            },
+        },
+    };
 </script>
