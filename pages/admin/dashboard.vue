0617c6975c344ec91dd6c0e6091cadbf7d7bddf1
John Wanjema <jonwanjema@gmail.com>
2024-02-18 09:46:23 -0800

    add placeholder content for dashboard

--- pages/admin/dashboard.vue
+++ pages/admin/dashboard.vue
@@ -1,5 +1,218 @@
 <template>
-    <div>
-      <h1>I am the dashboard</h1>
+   <main id="main-container" style="min-height: 346.5px;">
+
+<!-- Page Content -->
+<div class="content">
+    <div class="row js-appear-enabled animated fadeIn" data-toggle="appear">
+        <!-- Row #1 -->
+        <div class="col-6 col-xl-3">
+            <a class="block block-link-shadow text-right" href="javascript:void(0)">
+                <div class="block-content block-content-full clearfix">
+                    <div class="float-left mt-10 d-none d-sm-block">
+                        <i class="si si-bag fa-3x text-body-bg-dark"></i>
+                    </div>
+                    <div class="font-size-h3 font-w600 js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="1500">1500</div>
+                    <div class="font-size-sm font-w600 text-uppercase text-muted">Sales</div>
+                </div>
+            </a>
+        </div>
+        <div class="col-6 col-xl-3">
+            <a class="block block-link-shadow text-right" href="javascript:void(0)">
+                <div class="block-content block-content-full clearfix">
+                    <div class="float-left mt-10 d-none d-sm-block">
+                        <i class="si si-wallet fa-3x text-body-bg-dark"></i>
+                    </div>
+                    <div class="font-size-h3 font-w600">$<span data-toggle="countTo" data-speed="1000" data-to="780" class="js-count-to-enabled">780</span></div>
+                    <div class="font-size-sm font-w600 text-uppercase text-muted">Earnings</div>
+                </div>
+            </a>
+        </div>
+        <div class="col-6 col-xl-3">
+            <a class="block block-link-shadow text-right" href="javascript:void(0)">
+                <div class="block-content block-content-full clearfix">
+                    <div class="float-left mt-10 d-none d-sm-block">
+                        <i class="si si-envelope-open fa-3x text-body-bg-dark"></i>
+                    </div>
+                    <div class="font-size-h3 font-w600 js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="15">15</div>
+                    <div class="font-size-sm font-w600 text-uppercase text-muted">Messages</div>
+                </div>
+            </a>
+        </div>
+        <div class="col-6 col-xl-3">
+            <a class="block block-link-shadow text-right" href="javascript:void(0)">
+                <div class="block-content block-content-full clearfix">
+                    <div class="float-left mt-10 d-none d-sm-block">
+                        <i class="si si-users fa-3x text-body-bg-dark"></i>
+                    </div>
+                    <div class="font-size-h3 font-w600 js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="4252">4252</div>
+                    <div class="font-size-sm font-w600 text-uppercase text-muted">Online</div>
+                </div>
+            </a>
+        </div>
+        <!-- END Row #1 -->
     </div>
+    <div class="row js-appear-enabled animated fadeIn" data-toggle="appear">
+        <!-- Row #2 -->
+        <div class="col-md-6">
+            <div class="block">
+                <div class="block-header">
+                    <h3 class="block-title">
+                        Sales <small>This week</small>
+                    </h3>
+                    <div class="block-options">
+                        <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle" data-action-mode="demo">
+                            <i class="si si-refresh"></i>
+                        </button>
+                        <button type="button" class="btn-block-option">
+                            <i class="si si-wrench"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="block-content block-content-full">
+                    <div class="pull-all"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+                        <!-- Lines Chart Container functionality is initialized in js/pages/be_pages_dashboard.min.js which was auto compiled from _es6/pages/be_pages_dashboard.js -->
+                        <!-- For more info and examples you can check out http://www.chartjs.org/docs/ -->
+                        <canvas class="js-chartjs-dashboard-lines chartjs-render-monitor" width="1122" height="560" style="display: block; height: 280px; width: 561px;"></canvas>
+                    </div>
+                </div>
+                <div class="block-content">
+                    <div class="row items-push">
+                        <div class="col-6 col-sm-4 text-center text-sm-left">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">This Month</div>
+                            <div class="font-size-h4 font-w600">720</div>
+                            <div class="font-w600 text-success">
+                                <i class="fa fa-caret-up"></i> +16%
+                            </div>
+                        </div>
+                        <div class="col-6 col-sm-4 text-center text-sm-left">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">This Week</div>
+                            <div class="font-size-h4 font-w600">160</div>
+                            <div class="font-w600 text-danger">
+                                <i class="fa fa-caret-down"></i> -3%
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-4 text-center text-sm-left">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">Average</div>
+                            <div class="font-size-h4 font-w600">24.3</div>
+                            <div class="font-w600 text-success">
+                                <i class="fa fa-caret-up"></i> +9%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="block">
+                <div class="block-header">
+                    <h3 class="block-title">
+                        Earnings <small>This week</small>
+                    </h3>
+                    <div class="block-options">
+                        <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle" data-action-mode="demo">
+                            <i class="si si-refresh"></i>
+                        </button>
+                        <button type="button" class="btn-block-option">
+                            <i class="si si-wrench"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="block-content block-content-full">
+                    <div class="pull-all"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+                        <!-- Lines Chart Container functionality is initialized in js/pages/be_pages_dashboard.min.js which was auto compiled from _es6/pages/be_pages_dashboard.js -->
+                        <!-- For more info and examples you can check out http://www.chartjs.org/docs/ -->
+                        <canvas class="js-chartjs-dashboard-lines2 chartjs-render-monitor" width="1122" height="560" style="display: block; height: 280px; width: 561px;"></canvas>
+                    </div>
+                </div>
+                <div class="block-content bg-white">
+                    <div class="row items-push">
+                        <div class="col-6 col-sm-4 text-center text-sm-left">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">This Month</div>
+                            <div class="font-size-h4 font-w600">$ 6,540</div>
+                            <div class="font-w600 text-success">
+                                <i class="fa fa-caret-up"></i> +4%
+                            </div>
+                        </div>
+                        <div class="col-6 col-sm-4 text-center text-sm-left">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">This Week</div>
+                            <div class="font-size-h4 font-w600">$ 1,525</div>
+                            <div class="font-w600 text-danger">
+                                <i class="fa fa-caret-down"></i> -7%
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-4 text-center text-sm-left">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">Balance</div>
+                            <div class="font-size-h4 font-w600">$ 9,352</div>
+                            <div class="font-w600 text-success">
+                                <i class="fa fa-caret-up"></i> +35%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END Row #2 -->
+    </div>
+    <div class="row js-appear-enabled animated fadeIn" data-toggle="appear">
+        <!-- Row #3 -->
+        <div class="col-md-4">
+            <div class="block">
+                <div class="block-content block-content-full">
+                    <div class="py-20 text-center">
+                        <div class="mb-20">
+                            <i class="fa fa-envelope-open fa-4x text-primary"></i>
+                        </div>
+                        <div class="font-size-h4 font-w600">9.25k Subscribers</div>
+                        <div class="text-muted">Your main list is growing!</div>
+                        <div class="pt-20">
+                            <a class="btn btn-rounded btn-alt-primary" href="javascript:void(0)">
+                                <i class="fa fa-cog mr-5"></i> Manage list
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="block">
+                <div class="block-content block-content-full">
+                    <div class="py-20 text-center">
+                        <div class="mb-20">
+                            <i class="fa fa-twitter fa-4x text-info"></i>
+                        </div>
+                        <div class="font-size-h4 font-w600">+36 followers</div>
+                        <div class="text-muted">You are doing great!</div>
+                        <div class="pt-20">
+                            <a class="btn btn-rounded btn-alt-info" href="javascript:void(0)">
+                                <i class="fa fa-users mr-5"></i> Check them out
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="block">
+                <div class="block-content block-content-full">
+                    <div class="py-20 text-center">
+                        <div class="mb-20">
+                            <i class="fa fa-check fa-4x text-success"></i>
+                        </div>
+                        <div class="font-size-h4 font-w600">Business Plan</div>
+                        <div class="text-muted">This is your current active plan</div>
+                        <div class="pt-20">
+                            <a class="btn btn-rounded btn-alt-success" href="javascript:void(0)">
+                                <i class="fa fa-arrow-up mr-5"></i> Upgrade to VIP
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END Row #3 -->
+    </div>
+</div>
+<!-- END Page Content -->
+
+</main>
   </template>
