4dd6394c775ca306fe484c42dea5674adaa0444c
John Wanjema <jonwanjema@gmail.com>
2024-02-18 09:46:34 -0800

    show placeholder image

--- pages/admin.vue
+++ pages/admin.vue
@@ -39,14 +39,14 @@
               <div class="content-side content-side-full content-side-user px-10 align-parent">
                   <!-- Visible only in mini mode -->
                   <div class="sidebar-mini-visible-b align-v animated fadeIn">
-                      <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="" />
+                      <img class="img-avatar img-avatar32" :src="require('@/assets/media/avatars/avatar15.jpg')" alt="" />
                   </div>
                   <!-- END Visible only in mini mode -->
 
                   <!-- Visible only in normal mode -->
                   <div class="sidebar-mini-hidden-b text-center">
                       <a class="img-link" href="be_pages_generic_profile.html">
-                          <img class="img-avatar" src="assets/media/avatars/avatar15.jpg" alt="" />
+                          <img class="img-avatar" :src="require('@/assets/media/avatars/avatar15.jpg')" alt="" />
                       </a>
                       <ul class="list-inline mt-10">
                           <li class="list-inline-item">
