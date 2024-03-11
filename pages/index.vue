d2f33c9d8288c99a5df7706c25fe14ec7a76ef72
John Wanjema <jonwanjema@gmail.com>
2024-03-11 10:59:33 -0700

    remove register and forgot password links

--- pages/index.vue
+++ pages/index.vue
@@ -40,12 +40,6 @@
                                     <div class="col-12 mb-10">
                                         <button type="submit" class="btn btn-block btn-hero btn-noborder btn-rounded btn-alt-primary"><i class="si si-login mr-10"></i> Sign In</button>
                                     </div>
-                                    <div class="col-sm-6 mb-5">
-                                        <a class="btn btn-block btn-noborder btn-rounded btn-alt-secondary" href="op_auth_signup.html"> <i class="fa fa-plus text-muted mr-5"></i> New Account </a>
-                                    </div>
-                                    <div class="col-sm-6 mb-5">
-                                        <a class="btn btn-block btn-noborder btn-rounded btn-alt-secondary" href="op_auth_reminder.html"> <i class="fa fa-warning text-muted mr-5"></i> Forgot password </a>
-                                    </div>
                                 </div>
                             </form>
                         </div>
